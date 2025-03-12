935ee5a749232c52348c2856dabb7b11f15b7559
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-13 00:29:37 +0530

    expressjs pushing

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    database: "delta_app",
-    password: "mysql@123", // Replace with your actual MySQL password
+    database: "Delta_app",
+    password: "Rahmani@30", // Replace with your actual MySQL password
 });
 
 // Connect to MySQL
@@ -34,9 +34,25 @@ const getRandomUser = () => {
 app.get("/", (req, res) => {
     res.send("Welcome to the Delta App!");
 });
-
+// show route to get all users
+app.get("/users", (req, res) => {
+let q = `select * from users`;
+try{
+    connection.query(q, (err, results) => {
+        if (err) throw err;{
+            console.log(results);
+            res.send(results);
+        }
+        
+    });
+}
+catch(err){
+    console.error(err);
+   res.send("some err in db");
+}
+});
 // Start Server
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-});
+}); 
